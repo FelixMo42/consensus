3ba95825aa680fc585199af2a443a8fc772ee00e
Felix Moses <felix@tacocat.com>
2024-09-02 17:56:47 -0700

    auth part 11

--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -1,9 +1,9 @@
 export async function GET({ locals }) {
     const session = await locals.auth()
 
-    if (!session?.user?.userId) {
-        return new Response(null, { status: 401, statusText: "Unauthorized" })
-    }
+    // if (!session?.user?.userId) {
+    //     return new Response(null, { status: 401, statusText: "Unauthorized" })
+    // }
 
     return json(session)
 }
