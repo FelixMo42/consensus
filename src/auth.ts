82c91aa26d2ab015e36bce60f0e953eed374ed95
Felix Moses <felix@tacocat.com>
2024-09-02 20:49:55 -0700

    auth part 23

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,19 @@
 import { ORCID_CLIENT_ID, ORCID_CLIENT_SECRET } from '$env/static/private';
 import { SvelteKitAuth } from "@auth/sveltekit"
 
+console.log("HIIIII!")
+
 export const { handle, signIn, signOut } = SvelteKitAuth({
     providers: [{
         id: "orcid",
         name: "ORCID",
         type: "oidc",
+        issuer: "https://orcid.org/",
         wellKnown: "https://orcid.org/.well-known/openid-configuration",
         clientId: ORCID_CLIENT_ID,
         clientSecret: ORCID_CLIENT_SECRET,
         profile(profile) {
+            console.log("PROFILE", profile)
             return {
                 test: "hi",
                 ...profile
